30c110b0f401b94c8a5c1724d9d0fc8e1896a61a
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-15 16:04:41 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let neigung = 0
 basic.forever(function () {
-	
+    neigung = input.rotation(Rotation.Pitch)
+    basic.showNumber(neigung)
+    if (Math.abs(neigung) > 10) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.setLedColor(0xff0000)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.setLedColor(0x00ff00)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    }
 })
